fe1dce313c7db4d5aa1d071f9d48eb015c419e70
phelippemichel <phelippe.michel@gmail.com>
2024-01-12 15:48:25 -0300

    refactor/dependency inversion

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
+using DependencyStore;
+using DependencyStore.Extensions;
+using DependencyStore.Repositories;
+using DependencyStore.Repositories.Contracts;
+using DependencyStore.Services;
+using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSingleton<Configuration>();
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddSqlConnection(connStr);
+builder.Services.AddRepositories();
+builder.Services.AddServices();
 builder.Services.AddControllers();
 
 var app = builder.Build();
